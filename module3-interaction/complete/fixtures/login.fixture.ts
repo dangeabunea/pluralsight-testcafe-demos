8fb2f158a6670f80bd86abc899d7616790d4a1e4
Dan Geabunea <dangeabunea@hotmail.com>
2020-11-21 16:35:48 +0200

    Page interaction demo complete

--- module3-interaction/complete/fixtures/login.fixture.ts
+++ module3-interaction/complete/fixtures/login.fixture.ts
@@ -1,13 +1,1 @@
-import {Selector} from "testcafe";
-import {LoginHelper} from "../helpers/login.helper";
 
-fixture('LoginFixture')
-    .page('http://localhost:4200/login');
-
-test('should log in', async t => {
-
-});
-
-test('should log out', async t => {
-
-});
--- module3-interaction/complete/fixtures/notes.fixture.ts
+++ module3-interaction/complete/fixtures/notes.fixture.ts
@@ -1,12 +1,1 @@
-import {Selector} from "testcafe";
-import {LoginHelper} from "../helpers/login.helper";
 
-fixture('Notes Page').page('http://localhost:4200/notes');
-
-test('Should filter notes', async t=> {
-
-});
-
-test('Should delete note', async t=> {
-
-});
