89c147da1b4917e1277b7c55ea3e8b2b8c798f5e
Dan Geabunea <dangeabunea@hotmail.com>
2020-11-24 17:40:15 +0200

    Updates to M6

--- module5-advanced-techniques/complete/helpers/login.po.ts
+++ module5-advanced-techniques/complete/helpers/login.po.ts
@@ -7,8 +7,15 @@ export class LoginPageObject{
         const passwordInput = Selector('#password');
         const loginButton = Selector('button').withAttribute('type', 'submit');
 
-        const username = getFromArgs(process.argv.slice(2), 'u');
-        const password = getFromArgs(process.argv.slice(2), 'p');
+        let username = 'john.doe';
+        let password = 'password123';
+
+        // if started via node, read custom arguments
+        if(process.argv[0] === 'node' && process.argv[1] === 'main.ts') {
+            username = getFromArgs(process.argv.slice(2), 'u');
+            password = getFromArgs(process.argv.slice(2), 'p');
+        }
+
         await t.typeText(userNameInput, username);
         await t.typeText(passwordInput, password);
         await t.click(loginButton);
